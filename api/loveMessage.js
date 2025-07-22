0ec8cf8cd95795f9da2b224472a3ce355193a7f1
Asher Etherington <asherce21@gmail.com>
2025-07-22 10:54:46 -0700

    Update loveMessage.js

--- api/loveMessage.js
+++ api/loveMessage.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
             "I miss you so much Ashley❤️.",
             "I love you more than anything.",
             "You are the most gorgeous girl ever and I am so lucky to be dating you.",
-            "I want to give you forhead kisses till we fall asleep in eachothers arms.",
+            "I want to give you forehead kisses till we fall asleep in eachothers arms.",
             "I can't wait to be with you for the rest of my life and spend my nights sleeping \
             with you and my mornings waking up next to you.",
             "When I am not with you all I think about is how all my problems and stress would \
@@ -20,7 +20,15 @@ export default async function handler(req, res) {
             "You and I can do anything and everything together and can't wait for our future together.",
             "You are the most beautiful girl ever and you always make me so happy whenever I look at you.",
             "I always feel so happy everytime I open up my phone and get to see a picture of you. Sometimes \
-            I just stare at my lockscreen and think about how lucky I am and how much I miss you."
+            I just stare at my lockscreen and think about how lucky I am and how much I miss you.",
+            "All I can think about every day is you.",
+            "You make the good times amazing and the bad times good whenever you're near.",
+            "Making you laugh is the best thing I can ever do (even if I have to act really weird).",
+            "I love looking at pictures of you smiling they make me so happy and I'm so lucky that I am able to make you happy.",
+            "I really like all the pictures you draw you are so talented and creative and you really do impress me all the time.",
+            "I'm worried they'll take my skin.",
+            "I'm worried they can read my thoughts.",
+            "I'm worried they found her grave."
         ];
         return loveMessages[Math.floor(Math.random() * loveMessages.length)];
     }
